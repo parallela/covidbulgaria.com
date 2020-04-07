c83dafab6fe1b920b1f2fc6a9803fb7cf851edde
lubomir@lyrics.bg <lubomir@lyrics.bg>
2020-04-07 22:15:37 +0300

    Little SEO optimization

--- src/functions/TitleControl.js
+++ src/functions/TitleControl.js
@@ -3,7 +3,7 @@ export const setTitle = async () => {
     await fetch('https://raw.githubusercontent.com/COVID-19-Bulgaria/covid-database/master/Bulgaria/TotalsDataset.json', {
         method: 'GET',
     }).then(res => res.json()).then((result) => {
-        title = "COVID-19 Bulgarian Stats | Infected: " + result.infected + " / Cured: " + result.cured;
+        title = "COVID-19 Българска Статистика | Заразени: " + result.infected + " / Излекувани: " + result.cured;
     });
 
     return document.title = title;
